801fd683515692b7a614efe2f71bd19c194c5f07
Jelly-Pudding <steven.green05@gmail.com>
2026-09-19 16:18:53 +0100

    bump for 26.3.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.jellypudding"
-version = "1.2"
+version = "1.3"
 
 repositories {
     mavenCentral()
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    paperweight.paperDevBundle("26.2.build.+")
+    paperweight.paperDevBundle("26.3.build.+")
 }
 
 java {
